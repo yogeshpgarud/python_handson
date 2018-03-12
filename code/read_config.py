755d3dfdfa8f9104bca7a678ae724f1902ba52fd
Yogesh Garud <yogesh.p.garud@gmail.com>
2018-03-12 21:26:12 +0530

    add read config file

---
+++ code/read_config.py
@@ -0,0 +1,13 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '../config/', './test')))
+config_path = "../config/"
+
+print os.getcwd()
+print sys.path
+print os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+
+# data = json.load(open(config_path+'config.json'))
+
+# pprint(data)
